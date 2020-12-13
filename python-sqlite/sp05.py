91edffd7dd09d322fe2ea2755cf8fad33715e7f2
Ahmad Yoosofan <yoosofan@gmx.com>
2020-12-13 18:22:16 +0330

    date: Sun 13 Dec 2020 06:22:16 PM +0330

--- python-sqlite/sp05.py
+++ python-sqlite/sp05.py
@@ -21,12 +21,13 @@ def test2(c):
   c.execute("SELECT * FROM stocks WHERE symbol = '%s'" % symbol)
   print(c.fetchone())
     # Larger example that inserts many records at a time
+  """
   purchases = [('2006-03-28', 'BUY', 'IBM', 1000, 45.00),
                ('2006-04-05', 'BUY', 'MSFT', 1000, 72.00),
                ('2006-04-06', 'SELL', 'IBM', 500, 53.00),
               ]
   c.executemany('INSERT INTO stocks VALUES (?,?,?,?,?)', purchases)
-  
+  """
 def testDelete(c):
   c.execute("DELETE FROM stocks WHERE symbol='IBM';")
   c.execute("select * from stocks");
